dfffdfb50560e407c4dd9c7e62e72e780bc07764
schollsebastian <scholl.sebastian@gmx.at>
2021-03-24 16:00:10 +0100

    Add activity

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,9 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user!.tag}`);
+
+  client.user!.setActivity('to !pin', { type: 'LISTENING' })
+    .catch(console.error);
 });
 
 client.on('message', async message => {
